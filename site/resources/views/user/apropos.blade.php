da8d8a19ffe8724d67c25585a3dbaa55e18f2a0c
clem <remy.clementine@ifosup.wavre.be>
2021-02-23 23:40:51 +0100

    ajout page article + html apropos

---
+++ site/resources/views/user/apropos.blade.php
@@ -0,0 +1,62 @@
+@extends('layouts.default')
+
+@section('content')
+    <article class="apropospage col-12">
+        <div class="presentation">
+            <figure>
+                <img src="" alt=""></img>
+            </figure>
+            <p>
+                Passionnée par le bricolage depuis qu’elle est en âge de tenir un tournevis, Valentine Van Gestel s’est pourtant d’abord illustrée durant 20 ans dans une autre passion : le journalisme. 
+                En 2018, conscientisée par l’importance de changer de mode de vie urgemment, elle adopte les philosophies de la permaculture et du zéro-déchet. Grâce à ses 5 années de formation auprès de son acolyte Robert dans l’émission ‘Une Brique dans le Ventre’, elle développe sa passion pour l’upcycling. 
+                En 2020, elle trouve sa voie en réalisant toute la décoration d’intérieur du nouveau café-cantine Al&Greta en retapant avec amour des meubles destinés… aux encombrants ! 
+                En 2021, elle crée Glück Design, société de revalorisation de meubles oubliés, qui respecte ses valeurs de respect de l’environnement, en s’inscrivant dans l’économie circulaire, durable et éco-responsable ; et souligne l’humanité d’une seconde chance. 
+            </p>
+        </div>
+        <div class="galerie">
+            <h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </h3>
+            <div class="col-3">
+                <figure>
+                    <img src="" alt=""></img>
+                </figure>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="" alt=""></img>
+                </figure>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="" alt=""></img>
+                </figure>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="" alt=""></img>
+                </figure>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="" alt=""></img>
+                </figure>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="" alt=""></img>
+                </figure>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="" alt=""></img>
+                </figure>
+            </div>
+            <div class="col-3">
+                <figure>
+                    <img src="" alt=""></img>
+                </figure>
+            </div>
+        </div>
+    </article>
+@endsection

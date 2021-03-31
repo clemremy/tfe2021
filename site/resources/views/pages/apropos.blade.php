6a02276e695db8631abf2fec129d1e2ef1b6db3c
clem <remy.clementine@ifosup.wavre.be>
2021-03-31 23:37:14 +0200

    add scss page apropos/contact

--- site/resources/views/pages/apropos.blade.php
+++ site/resources/views/pages/apropos.blade.php
@@ -2,50 +2,65 @@
 
 @section('content')
     <article class="apropospage col-12">
-        <div class="title">
-            <h1>Glück Design</h1>  <br />
-            <h1>Souligner l'humanité d'une seconde chance.</h1>
-        </div>
-        <div class="history">
-            <div class="author">
-                <figure>
-                    <img src="{{ asset('images/1.jpg') }}" alt="" style="width: 300px;"></img>
-                </figure>
-                <p class="rotatingtext">
-                    fondatrice - fondatrice -
-                </p>
-            </div>
-            <div class="text">
-                <p>
-                    Passionnée par le bricolage depuis qu’elle est en âge de tenir un tournevis, Valentine Van Gestel s’est pourtant d’abord illustrée durant 20 ans dans une autre passion : le journalisme. 
-                    En 2018, conscientisée par l’importance de changer de mode de vie urgemment, elle adopte les philosophies de la permaculture et du zéro-déchet. Grâce à ses 5 années de formation auprès de son acolyte Robert dans l’émission ‘Une Brique dans le Ventre’, elle développe sa passion pour l’upcycling. 
-                    En 2020, elle trouve sa voie en réalisant toute la décoration d’intérieur du nouveau café-cantine Al&Greta en retapant avec amour des meubles destinés… aux encombrants ! 
-                    En 2021, elle crée Glück Design, société de revalorisation de meubles oubliés, qui respecte ses valeurs de respect de l’environnement, en s’inscrivant dans l’économie circulaire, durable et éco-responsable ; et souligne l’humanité d’une seconde chance. 
-                </p>
+        <div class="first-slide">
+            <div class="img-background"></div>
+            <span class="line one"></span>
+            <span class="line two"></span>
+            <div class="title">
+                <h1>
+                    Glück Design  
+                    <br />
+                    Souligner l'humanité d'une seconde chance.</h1>
             </div>
         </div>
-        <div class="galerie">
-            <div>
-                <figure class="small">
-                    <img src="{{ asset('images/1.jpg') }}" />
-                </figure>
-                <figure class="big">
-                    <img src="{{ asset('images/1.jpg') }}" />
-                </figure>
+        <div class="container">
+            <div class="history">
+                <div class="author col-6">
+                    <h2>
+                        Valentine
+                    </h2>
+                    <figure>
+                        <img src="{{ asset('images/1.jpg') }}" alt="" ></img>
+                    </figure>
+                    <p class="rotatingtext">
+                        fondatrice - fondatrice -
+                    </p>
+                </div>
+                <div class="text col-6">
+                    <p>
+                        Passionnée par le bricolage depuis qu’elle est en âge de tenir un tournevis, Valentine Van Gestel s’est pourtant d’abord illustrée durant 20 ans dans une autre passion : le journalisme. 
+                        <br /><br />
+                        En 2018, conscientisée par l’importance de changer de mode de vie urgemment, elle adopte les philosophies de la permaculture et du zéro-déchet. Grâce à ses 5 années de formation auprès de son acolyte Robert dans l’émission ‘Une Brique dans le Ventre’, elle développe sa passion pour l’upcycling. 
+                        <br /><br />
+                        En 2020, elle trouve sa voie en réalisant toute la décoration d’intérieur du nouveau café-cantine Al&Greta en retapant avec amour des meubles destinés… aux encombrants ! 
+                        <br /><br />
+                        En 2021, elle crée Glück Design, société de revalorisation de meubles oubliés, qui respecte ses valeurs de respect de l’environnement, en s’inscrivant dans l’économie circulaire, durable et éco-responsable ; et souligne l’humanité d’une seconde chance. 
+                    </p>
+                </div>
             </div>
-            <div>
-                <figure class="big">
-                    <img src="{{ asset('images/1.jpg') }}" />
-                </figure>
-                <figure class="small">
-                    <img src="{{ asset('images/1.jpg') }}" />
-                </figure>
+            <div class="galerie">
+                <ul class="col-12">
+                    <li class="col-4">
+                        <img src="{{ asset('images/1.jpg') }}" />
+                    </li>
+                    <li class="col-4">
+                        <img src="{{ asset('images/2b.jpg') }}" />
+                    </li>
+                </ul>
+                <ul>
+                    <li class="col-4">
+                        <img src="{{ asset('images/3.jpg') }}" />
+                    </li>
+                    <li class="col-4">
+                        <img src="{{ asset('images/1.jpg') }}" />
+                    </li>
+                </ul>
             </div>
+            <form class="newsletter" >
+                <label>Restez informé</label>
+                <br/>
+                <input type="email" name="email" value="" placeholder="catherinemoulin@gmail.com"  ><a></a>
+            </form>
         </div>
-        <form class="newsletter">
-            <label>Restez informé</label>
-            <br/>
-            <input type="email" name="email" value="" placeholder="catherinemoulin@gmail.com">
-        </form>
     </article>
 @endsection
--- site/resources/views/pages/contact.blade.php
+++ site/resources/views/pages/contact.blade.php
@@ -2,51 +2,54 @@
 
 @section('content')
     <article class="contactpage">
-        <div class=image>
-            <img src="{{ asset('images/1.jpg') }}" style="height: 100px"/>
-        </div>
-        <div class="title">
-            <h1>
-                Une question?
-            </h1>
-        </div>
-        <form class="contact">
-            <p>
-                <label for="prenom">Prénom:</label>
-                <input type="text" name="prenom" placeholder="Jean-Claude" required>
-            </p>
-            <p>
-                <label for="nom">Nom:</label>
-                <input type="text" name="nom" placeholder="Van Damme" required>
-            </p>
-            <p>
-                <label for="email">Adresse mail:</label>
-                <input type="text" name="email" placeholder="jcvd@mail.com" required>
-            </p>
-            <p>
-                <label for="sujet">Sujet:</label>
-                <select name="sujet">
-                    <option value="">--Choisir une option--</option>
-                    <option value="rdv">Rendez-vous</option>
-                    <option value="commande">Commande personnalisée</option>
-                    <option value="sav">Service après vente</option>
-                    <option value="autre">Autre</option>
-                </select>
-            </p>
-            <p>
-                <label for="message">Message:</label>
-                <input type="textarea" name="message" placeholder="Bonjour, j'aimerais..." required>
-            </p>
-        </form>
-        <div class="info">
-            <div>
-                <i class="fa-lg"></i>
-                <a href="mailto:gluckdesign@gmail.com">gluckdesign@gmail.com</a>
-                <a href="tel:+32000000000">+32 000/000.000</a>
+    <div class="first-slide">
+            <div class="img-background"></div>
+            <span class="line one"></span>
+            <span class="line two"></span>
+            <div class="title">
+                <h1>
+                    Une question?</h1>
             </div>
-            <div>
-                <i class="fa-lg"></i>
-                <a href="https://goo.gl/maps/uXqaj33p7eq69ZSw5">adresse</a>
+        </div>
+        <div class="container">
+            <form class="contact">
+                <p>
+                    <label for="prenom">Prénom:</label>
+                    <input type="text" name="prenom" placeholder="Jean-Claude" required>
+                </p>
+                <p>
+                    <label for="nom">Nom:</label>
+                    <input type="text" name="nom" placeholder="Van Damme" required>
+                </p>
+                <p>
+                    <label for="email">Adresse mail:</label>
+                    <input type="text" name="email" placeholder="jcvd@mail.com" required>
+                </p>
+                <p>
+                    <label for="sujet">Sujet:</label>
+                    <select name="sujet">
+                        <option value="">--Choisir une option--</option>
+                        <option value="rdv">Rendez-vous</option>
+                        <option value="commande">Commande personnalisée</option>
+                        <option value="sav">Service après vente</option>
+                        <option value="autre">Autre</option>
+                    </select>
+                </p>
+                <p>
+                    <label for="message">Message:</label>
+                    <input type="textarea" name="message" placeholder="Bonjour, j'aimerais..." required>
+                </p>
+            </form>
+            <div class="info">
+                <div>
+                    <i class="fa-lg"></i>
+                    <a href="mailto:gluckdesign@gmail.com">gluckdesign@gmail.com</a>
+                    <a href="tel:+32000000000">+32 000/000.000</a>
+                </div>
+                <div>
+                    <i class="fa-lg"></i>
+                    <a href="https://goo.gl/maps/uXqaj33p7eq69ZSw5">adresse</a>
+                </div>
             </div>
         </div>
     </article>

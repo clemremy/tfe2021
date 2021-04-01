2604a7cf9abb383875e73b2765474e9ac8c5aff1
clem <remy.clementine@ifosup.wavre.be>
2021-04-01 23:37:47 +0200

    add footer scss

--- site/resources/views/includes/footer.blade.php
+++ site/resources/views/includes/footer.blade.php
@@ -1,43 +1,69 @@
 <div class="footer">
-    <nav class="footernav">
-        <ul class="col-12">
-            <div class="col-4">
-                <li>
-                    <a href="https://www.facebook.com/"><i class="fab fa-facebook fa-lg"></i></a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com/"><i class="fab fa-instagram fa-lg"></i></a>
-                </li>
-            </div>
-            <div class="col-8">
-                <li>
-                    <a href="{{ url('/apropos') }}">A propos</a>
-                </li>
-                <li>
-                    <a href="{{ url('/contact') }}">Contact</a>
-                </li>
-                <li>
-                    <a href="/user/ateliers">Ateliers</a>
-                </li>
-                <li>
-                    <a href="/user/produits">Produits</a>
-                </li>
-            </div>
+    <nav class="footer-nav col-8">
+        <ul>
+            <li>
+                <p>01</p>
+                <a href="/">Accueil</a>
+            </li>
+            <li>
+                <p>02</p>
+                <a href="{{ url('/apropos') }}">A propos</a>
+            </li>
+            <li>
+                <p>03</p>
+                <a href="{{ url('/contact') }}">Contact</a>
+            </li>
+            <li>
+                <p>04</p>
+                <a href="/user/ateliers">Atelier</a>
+            </li>
+            <li>
+                <p>05</p>
+                <a href="/user/mobilier">Mobilier</a>
+            </li>
         </ul>
     </nav>
-    <nav class="mentions">
-        <ul class="col-12">
-            <li class="col-3">
-                &copy; Glück Design 2021
+    <div class="footer-info col-4">
+        <div class="social">
+            <p>Nous suivre</p>
+            <a href="https://www.facebook.com/Gl%C3%BCck-Design-101383345315020/?ref=page_internal">Facebook</a>
+            <br/>
+            <a href="">Instagram</a>
+        </div>
+        <div class="contact">
+            <p>Contact</p>
+            <address>
+                <a href="mailto:gluckdesign@gmail.com">
+                    gluckdesign@gmail.com
+                </a>
+                <br/>
+                <a href="tel:+32 473 930 226">
+                    +32 473 930 226
+                </a>
+            </address>
+        </div>
+        <div class="adresse">
+            <p>Adresse</p>
+            <address>
+                28 Rue des Hannetons
+                <br/>
+                1170 Boitsfort
+                <br/>
+                Belgium
+            </address>
+        </div>
+    </div>
+    <nav class="mentions col-12">
+        <ul>
+            <li>
+                &copy; Glück Design 2021 |
+                <a href=""><strong>Clem Remy</strong></a>
             </li>
-            <li class="col-3">
+            <li>
                 <a href="">Politiques de confidéntialité</a>
             </li>
-            <li class="col-3">
+            <li>
                 <a href="">Conditions générales de ventes</a>
-            </li>
-            <li class="col-3">
-                <a href="">Création web : <strong>Clem Remy</strong></a>
             </li>
         </ul>
     </nav>
